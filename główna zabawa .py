2bb7ce6701c4f4e7078583be3f050ef890a42a61
GplJakubP <115486348+GplJakubP@users.noreply.github.com>
2025-12-31 09:02:51 +0100

    Add files via upload

--- główna zabawa .py
+++ główna zabawa .py
@@ -150,3 +150,103 @@
 #   "Tomato":Product(),
 # }
 # print(dict)
+
+
+
+# 31.12.2025
+
+# konstruktor i pola obiektu 
+
+# class Student:
+#     def __init__(self):
+# #         print("Powstaje nowy obiekt ")
+# # if __name__ == '__main__':
+# #     student = Student()
+#         self.first_name = "Jakbu"
+#         self.last_name = "Piejko"
+#         self.age = 23
+# def run_example():
+#     student = Student()
+#     print(student.first_name)
+#     print(student.last_name)
+#     print(student.age)
+# if __name__ == '__main__':
+#     run_example()
+    
+#  możemy modyfikować zmiennie za pomocą odwołania się do nich 
+# student.first_name = ""
+
+
+
+# Zadanie 1-3 
+
+# import random
+
+# class Product:
+#     def __init__(self, name, category_name, unit_price):
+#         self.name = name
+#         self.category_name = category_name
+#         self.unit_price = unit_price
+
+# class Order:
+#     def __init__(self, first_name, last_name, products_list=None):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         if products_list is None:
+#             products_list = []
+#         self.products_list = products_list
+#         self.summary_price = calculate_order(products_list)
+# class Apple:
+#     def __init__(self, species_name, size, price_per_kg):
+#         self.species_name = species_name
+#         self.size = size
+#         self.price_per_kg = price_per_kg
+
+# class Potato:
+#     def __init__(self, species_name, size, price_per_kg):
+#         self.species_name = species_name
+#         self.size = size
+#         self.price_per_kg = price_per_kg
+
+# def calculate_order(products_list):
+#     total_price = 0
+#     for product in products_list:
+#         total_price += product.unit_price
+#     return total_price  # return poza pętlą!
+
+# def generate_random_products():
+#     number_of_products = random.randint(1, 15)
+#     products = []
+#     for product_number in range(number_of_products):
+#         name = f"Produkt-{product_number + 1}"
+#         category = f"Kategoria-{product_number + 1}"
+#         unit_price = random.randint(1, 8)
+#         products.append(Product(name, category, unit_price))
+#     return products  # return poza pętlą!
+
+# def print_list_of_products(products):
+#     for product in products:
+#         print(f"Produkt: {product.name}, Kategoria: {product.category_name}, Cena jednostkowa: {product.unit_price} zł")
+
+# def print_order_summary(order):
+#     print("Zamówienie dla:", order.first_name, order.last_name)
+#     print("Lista produktów:")
+#     print_list_of_products(order.products_list)  # wykorzystujemy funkcję wypisującą produkty
+#     print("Cena całkowita:", order.summary_price, "zł")
+#     print("-" * 40)
+
+# def run_example():
+#     # Ręczne zamówienie
+#     apple = Product("Jabłko", "Owoce", 2)
+#     potato = Product("Ziemniak", "Warzywa", 3)
+#     order1 = Order("Jakub", "P", [apple, potato])
+#     print_order_summary(order1)
+
+#     # Losowe zamówienie
+#     random_products = generate_random_products()
+#     order2 = Order("Jakub", "Piejko", random_products)
+#     print_order_summary(order2)
+
+# if __name__ == '__main__':
+#     run_example()
+
